de39c8609102a5fd635b7ed23538819a99ad8409
杨福照 <yangfuzhao@ipaynow.cn>
2017-11-16 15:37:16 +0800

    1

--- model_auth/example/CheckTest.php
+++ model_auth/example/CheckTest.php
@@ -8,23 +8,23 @@
 require_once '../services/Service.php';
 
 //身份认证
-//print Services::toCheckID("1441071499740581","DK96gnOB7EmVDDaHgLTLEZqVgP0H0nML","abnMX6YXHjBdesCsn2TD8b25","checkId1","xxxxxxxxxxxxxxxxxxxxxx","xxx");
+//print Services::toCheckID("xxxxxxxxxxxxxxxxxx","xxxxxxxxxxxxxxxxxxxxx","xxxxxxxxxxxxxxxx","checkId1","xxxxxxxxxxxxxxxxxxxxxx","xxx");
 
 //身份认证查询
-//print Services::queryCheckID("1441071499740581","DK96gnOB7EmVDDaHgLTLEZqVgP0H0nML","abnMX6YXHjBdesCsn2TD8b25","checkId1");
+//print Services::queryCheckID("xxxxxxxxxxxxxxxxxxxx","xxxxxxxxxxxxxxxxxxxxx","xxxxxxxxxxxxxxxxxx","checkId1");
 
 //卡信息认证 M2M1ZmRjNjc1MmYxNmRiNWNhY2NmMmIzYzk1ZGZlZTI=
 //            Y2IwYjRiYTQ1MTUyOTQ4YTQyNzJhZDBhYjIxZDNkZjI=
-print Services::toCheckCard("1441071499740581","DK96gnOB7EmVDDaHgLTLEZqVgP0H0nML","Z5CbXbEaJA6CYRSDS5Ddsab6","phpCardbeijin0","xxxxxxxxxxxxxxxxxxxxx","xxx","01","xxxxxxx","xxxxx");
+//print Services::toCheckCard("xxxxxxxxxxxxxxxxxxxxxxxxxx","xxxxxxxxxxxxxxxxxxxx","xxxxxxxxxxxxxxxxxxxx","phpCardbeijin2","xxxxxxxxxxxxxxxxxxxxx","xxx","01","xxxxxxx","xxxxx");
 //
 //卡信息认证查询
-//print Services::queryCheckCard("1441071499740581","DK96gnOB7EmVDDaHgLTLEZqVgP0H0nML","Z5CbXbEaJA6CYRSDS5Ddsab6","phpCard0");
+//print Services::queryCheckCard("xxxxxxxxxxxxxxxxxxxxxxxx","xxxxxxxxxxxxxx","xxxxxxxxxxxxxxxxxxx","phpCard0");
 
 
-//print Services::toCheckMobileNo("1476788813528909","c4MlX9GJCi0YI5z3RvpK17wPlscFKpY1",
-//"dYTAwfNMEeT3pk5FB8MS5MYk","phpMobile1","xxxxxxxxxxxxx","xxxx","01","xxxxxx");
+print Services::toCheckMobileNo("xxxxxxxxxxxxxxxxxxxx","xxxxxxxxxxxxxxxxxxxxxxx",
+"xxxxxxxxxxxxxxxxxxxx","phpMobile1","xxxxxxxxxxxxx","xxxx","01","xxxxxx");
 //
 //手机号认证查询
-//print Services::queryCheckMobileNo("1476788813528909","c4MlX9GJCi0YI5z3RvpK17wPlscFKpY1","dYTAwfNMEeT3pk5FB8MS5MYk","phpMobile1");
+//print Services::queryCheckMobileNo("xxxxxxxxxxxxxxxxxxxxxxxxx","xxxxxxxxxxxxxxxxxxxxxxxxx","xxxxxxxxxxxxxxxxxxxxxxx","phpMobile1");
 
 
